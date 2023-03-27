b01892551c219dc1ca82e989b2ee2a9f4c72f158
YasminBatista <yasminbdsouza83@gmail.com>
2023-03-27 10:27:29 -0300

    oii

---
+++ src/components/ButtonSlider/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native/types";
+import { colors } from '../../styles/colors';
+
+export const styles = StyleSheet.create({
+  ball: {
+    width: 30,
+    height: 30,
+    backgroundColor: colors.third,
+    borderRadius: 15
+  }
+
+})
---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    primary: '#487552',
+    third: '#D9D9D9',
+    black: '#000',
+    white: '#FFF'
+
+}
