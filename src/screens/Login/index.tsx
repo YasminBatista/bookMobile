0dd0291c63e21dbef0edf45ddf23c1c542373474
YasminBatista <yasminbdsouza83@gmail.com>
2023-09-04 10:32:56 -0300

    0409

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,9 @@
- import React from "react"
+ import React, { useState } from "react"
  import { 
         View,
         Text,
-        KeyboardAvoidingView
+        KeyboardAvoidingView,
+        Alert
      } from "react-native"
 import {MaterialIcons} from '@expo/vector-icons';
 import {styles} from "./styles"
@@ -11,8 +12,29 @@ import { colors } from "../../styles/colors"
 import { Foundation } from '@expo/vector-icons'; 
 import {ComponentButtonInterface} from "../../components"
 import { LoginTypes } from "../../navigations/login.navigation";
- 
- export function Login({navigation}: LoginTypes){
+import { Axios,AxiosError} from "axios";
+import { useAuth } from "../../hooks/auth";
+import { IAuthenticate } from "../../services/data/User";
+export function Login({navigation}:LoginTypes) {
+    const { signIn } = useAuth();
+    const [data,setData] = useState<IAuthenticate>();
+    const [isLoading, setIsLoading] = useState(true);
+    async function handleSignIn(){
+        try{
+            setIsLoading(true);
+            if(data?.email && data.password){
+                await signIn(data);
+            } else {
+                Alert.alert("Preencha todos os campos");
+                setIsLoading(false);
+            }
+        }catch(error){
+            const err = error as AxiosError;
+            const message = err.response?.data as string
+            Alert.alert(message)
+            setIsLoading(false)
+        }
+    }
     return (
         <View style={styles.container}>
             <KeyboardAvoidingView>
